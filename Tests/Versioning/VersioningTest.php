9bf66b172c303455b8bf41e5f1ef66f9d37e1db9
nathanvss <nathan.vasse@gmail.com>
2016-12-22 15:47:54 +0100

    init phpunit

---
+++ Tests/Versioning/VersioningTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathan
+ * Date: 22/12/2016
+ * Time: 15:41
+ */
+
+namespace Vss\UsefulBundle\Tests\Versionning;
+
+class VersioningTest extends \PHPUnit_Framework_TestCase {
+
+    public function test() {
+        
+    }
+
+}
---
+++ Utils/Versioning/Versioning.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathan
+ * Date: 22/12/2016
+ * Time: 15:42
+ */
+
+namespace Vss\UsefulBundle\Utils\Versioning;
+
+/**
+ * Class Versioning
+ * @package Vss\UsefulBundle\Utils\Versioning
+ */
+class Versioning {
+
+    static function toInt($version) {
+        
+    }
+
+}
